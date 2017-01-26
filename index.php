2cc9ae81708869ef861a306ab73a2d14cc66823d
ophir9 <ophir.impact@gmail.com>
2017-01-26 21:25:16 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,341 @@
+<html ng-app="vApp">
+<head>
+    <title>Vertical technologies</title>
+
+    <!-- Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+
+    <!-- JavaScript for: jQuery, angular, materialize, and angular-materialize. All of which are needed. -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-route.js"></script>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/angular-materialize/0.2.1/angular-materialize.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+<link href="https://fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+
+<link href="https://fonts.googleapis.com/css?family=Carrois+Gothic+SC" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Carrois+Gothic+SC|Dosis+Oxygen+Quicksand+Rubik" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="dist/css/unslider.css">
+
+    <!-- Your app -->
+</head>
+<body>
+    <nav class="white z-depth-0">
+    <div class="nav-wrapper white">
+      <div class="container">
+          <div class="wow">
+              <a href="#!" class="brand-logo orange-text" style="font-family: 'Julius Sans One', sans-serif;">Verticle </a>
+
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down ">
+        <li ><a class="" href="#">About us</a></li>
+        <li ><a class="" href="#">Services</a></li>
+        <li ><a class="" href="#">OEM partners</a></li>
+        <li ><a class="" href="#">Partners</a></li>
+        <li ><a class="" href="#">RMA process</a></li>
+        <li ><a class="" href="#">Careers</a></li>
+        <li ><a class="" href="#">Contact us</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a class="" href="#">About us</a></li>
+        <li><a class="" href="#">Services</a></li>
+        <li><a class="" href="#">OEM partners</a></li>
+        <li><a class="" href="#">Partners</a></li>
+        <li ><a class="" href="#">RMA process</a></li>
+        <li ><a class="" href="#">Careers</a></li>
+        <li ><a class="" href="#">Contact us</a></li>
+      </ul> -->
+          </div>
+      </div>
+    </div>
+  </nav>
+
+<div class="container-fluid" id="l_con">
+    <div class="landing_block" >
+        <div class="" id="white_overlay">
+            
+            <div class="container">
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row">
+                    <div class="col l12 s12 m12">
+                        <div class="landing-text" id="typewriter">
+
+                    <h1 class="white-text  animated bounceInDown" style="
+
+    font-family: 'Julius Sans One', sans-serif;
+
+">Welcome to <br>
+                    Verticle Technologies</h1>
+                <h5 class="white-text flow-text  animated bounceInUp" id="element" >
+                    Your search for IT INFRASTRUCTURE, NETWORK SERVICES, DIGITAL COMMUNICATIONS, SECURITY AND SURVEILLANCE, VOICE AND VIDEO CONNECTIVITY, STORAGE SERIVCES and SWITCHES AND SMART ROUTING ENDS HERE
+
+                </h5>
+            </div>
+                    </div>
+                </div>
+
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row">
+                    
+                </div>
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row"></div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid" id="con">
+    
+    <div class="container">
+<div class="row">
+    <div class="column">
+        <div class="col s12 l12 m12 center">
+            <h1 class="flow-text center" style="color: #000;">
+                Services and Solutions
+            </h1>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="column">
+
+        <div class="col s12 l4 m6 center">
+            <h1 class="flow-text">IT Infrastructure</h1>
+            <span class="center"><i class="small material-icons center">business</i></span>
+            
+            <p class="flow-text">
+                Our IT Infra Services will transform your IT platform with effective infrastructure management solutions.
+            </p>
+
+        </div>
+
+        <div class="col s12 l4 m6 center">
+            <h1 class="flow-text">Network Services</h1>
+            <span class="center"><i class="small material-icons center">settings_input_antenna</i></span>
+            
+            <p class="flow-text">
+                We leverage 20 year of networking experience to deliver an effective global network infrastructure.
+            </p>
+
+        </div>
+
+        <div class="col s12 l4 m6 center">
+            <h1 class="flow-text">Wireless Networking</h1>
+            <span class="center"><i class="small material-icons center">settings_remote</i></span>
+            
+            <p class="flow-text">
+                TelExcell helps ensure the appropriate level of auditability for enterprise wireless LAN deployments.
+            </p>
+
+        </div>
+    </div>
+
+    <div class="column">
+
+        <div class="col s12 l4 m6 center">
+            <h1 class="flow-text">Digital Communication</h1>
+            <span class="center"><i class="small material-icons center">theaters</i></span>
+            <p class="flow-text">
+                We are focused on providing system-level solutions that enable accelerated design cycles, & lower costs.
+            </p>
+
+        </div>
+
+        <div class="col s12 l4 m6 center">
+            <h1 class="flow-text">Security & Surveillance</h1>
+            <span class="center"><i class="small material-icons center">my_location</i></span>
+            <p class="flow-text">
+                We provide mission critical solutions that span access control, security management & surveillance solutions.
+            </p>
+
+        </div>
+
+        <div class="col s12 l4 m6 center">
+            <h1 class="flow-text">Data Communication</h1>
+            <span class="center"><i class="small material-icons center">settings_cell</i></span>
+            <p class="flow-text">
+                We delivers managed communications solutions to simply & securely interact & transact with other industries.
+            </p>
+
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="col s12 m6 l6 center">
+            <h1 class="flow-text">Switches & Routers</h1>
+            <span class="center"><i class="small material-icons center">settings_input_composite</i></span>
+            <p class="flow-text">
+                We provide a family of switching & routing products that exceeds all prospects for building the network needed.
+            </p>
+        </div>
+
+        <div class="col s12 m6 l6 center">
+            <h1 class="flow-text">Voice & Video</h1>
+            <span class="center"><i class="small material-icons center">video_library</i></span>
+            <p class="flow-text">
+                We provide rich-media skills as secured high - density voice & video connectivity, conference & transcoding.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="column">
+        <div class="col s12 l12 m12 center">
+           <h3 class="black-text flow-text">
+               Then, now and next....
+           </h3>
+        </div>
+
+        <div class="col s12 l12 m12 ">
+            <ul class="collapsible " data-collapsible="expandable">
+    <li>
+      <div class="collapsible-header active"><i class="material-icons">assignment</i><h4>Our History</h4></div>
+      <div class="collapsible-body"><p class="flow-text ">
+In 1993, TelExcell entered the growing telecommunications market selling dial-up modems and as the technology matured, got into the wireless space by upgrading to the latest technologies. From a small size operation, TelExcell has grown to be a dynamic enterprise with over 150+ employees and 15 locations. We have continually expanded services in response to customer needs.
+</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">mode_edit</i><h4>What we are doing now</h4></div>
+      <div class="collapsible-body"><p class="flow-text ">
+For over 21 years, we've been dedicated to helping businesses use technology to increase productivity & profits. We are a trusted advisor for all your IT / Telecom needs, including telephone systems, carrier services, voice/ video/data networks, IP Surveillance, Internet services, structured cabling and IT services..
+</p></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">trending_up</i><h4>Verticle in future</h4></div>
+      <div class="collapsible-body"><p class="flow-text ">
+          
+We believe that the company's strength lies in its core values such as integrity, honesty, commitment, team work, and professionalism and that these will drive the growth in the future.
+
+      </p></div>
+    </li>
+    
+  </ul>
+        
+        </div>
+    </div>
+
+</div>
+
+
+
+</div>
+
+
+</div>
+
+<div class="container-fluid" id="l_con">
+    <div class="landing_block" >
+        <div class="" id="white_overlay">
+            
+            <div class="container">
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row">
+                    <h1 class="flow-text white-text">Client says ...</h1>
+                </div>
+                <div class="row">
+                    <div class="col l12 s12 m12">
+                        <div class="landing-text" id="typewriter">
+
+                    
+                <blockquote class="white-text flow-text " id="" >
+                   
+                    “Everything went extremely well, was completed on the expected schedule and they had 0 down time. I had all the information I needed to do the service switch over during the appointed time, and it went flawlessly. This set up was a testament to the skill of your team there at TelExcell. I will not hesitate to recommend your services wherever there is an opportunity." 
+
+                </blockquote>
+            </div>
+                    </div>
+                </div>
+
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row">
+                    
+                </div>
+                <div class="row"></div>
+                <div class="row"></div>
+                <div class="row"></div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div id="con">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col s12 l12 m12 ">
+            <div class="container center">
+                <h1 class="flow-text">About us</h1>
+                <p class="flow-text" style="text-align: left;">
+                    Verticle is a trusted advisor for all your IT / Telecom needs, including telephone systems, carrier services, voice / video / data networks, IP Surveillance, Internet services, structured cabling and IT services.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+   <footer class="page-footer black">
+          <div class="container black">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text flow-text">Verticle technologies</h5>
+                <p class="grey-text text-lighten-4"></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"></h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!"></a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"></a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"></a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2017 Verticle Technologies
+            <a class="grey-text text-lighten-4 right" href="#!"></a>
+            </div>
+          </div>
+        </footer>
+    <script src="dist/js/unslider-min.js"></script> <!-- but with the right path! -->
+</body>
+<script type="text/javascript">
+    $(document).ready(function(){
+    $('.collapsible').collapsible();
+    $('.my-slider').unslider({
+        autoplay: true
+    });
+  });
+</script>
+
+<script type="text/javascript">
+    angular.module('vApp', ['ngRoute']).config(function($routeProvider){
+    	$routeProvider.when("/", {
+    		url: "home",
+    		template:"<h1> Wow </h1>";
+    	});
+    });
+</script>
+</html>
